faf2dd42615ee1f889d760bd98728a30d0d9dd56
SanishPoudel <mr.sanish.poudel@gmail.com>
2024-11-13 12:53:42 +1100

    Basic structuring, added player and gameboard funciton

---
+++ script.js
@@ -0,0 +1,41 @@
+// basic structure and requirements
+// 2 players: x & o
+// board[3][3] 
+// isfull = false
+// loop board 
+// {
+//   if the board is full
+//   {
+//     isfull = true
+//   }
+//   check for wins()
+//   check for draws() 
+//   play next round() 
+// }
+
+// factory function to make players
+function Player(name) // name can be x or o
+{
+  return 
+  {
+    name
+  };
+}
+
+// factory function to display gameboard
+function Gameboard()
+{
+  const gameboard = [
+    [
+      [],[],[]
+    ],
+    [
+      [],[],[]
+    ],
+    [
+      [],[],[]
+    ],
+  ];
+
+  return  gameboard;
+}
