b76b1397f9514e418952bdd739470517f453fcd9
SanishPoudel <mr.sanish.poudel@gmail.com>
2024-11-15 11:28:14 +1100

    removed the players function

--- script.js
+++ script.js
@@ -1,26 +1,3 @@
-// basic structure and requirements
-// 2 players: x & o
-// board[3][3] 
-// isfull = false
-// loop board 
-// {
-//   if the board is full
-//   {
-//     isfull = true
-//   }
-//   check for wins()
-//   check for draws() 
-//   play next round() 
-// }
-
-// factory function to make players
-function player(name) // name can be x or o
-{
-  return {
-    name
-  };
-}
-
 // factory function to display gameboard
 function displayBoard() {
   const board = [
@@ -45,5 +22,5 @@ const gameboard = displayBoard();
 // playRound() takes the row, column and player(x or o) and updates the board
 function playRound(player, row, column) {
     gameboard.board[row][column] = player;
-    return gameboard.board
+    return gameboard.board;
 }
